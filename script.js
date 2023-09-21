2ffb5623c92437e5fc4717c0badb21a440d49fd7
Heang Seavleu <seavleuheang@gmail.com>
2023-09-21 23:03:01 +0900

    responsiveness

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const menu = document.getElementById("menu");
 const menu_icon = document.getElementById("menu_icon");
 
-menu_icon.addEventListener("click", () =>{
-    menu.classList.toggle("toggleMenu")
-})
+menu_icon.addEventListener("click", () => {
+  menu.classList.toggle("toggleMenu");
+});
